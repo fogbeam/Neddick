1127ca848ef134d74f5b33bc6709d0c7308e8d49
Phillip Rhodes <motley.crue.fan@gmail.com>
2011-11-30 18:55:39 -0500

    adds file with sql for blowing away all Entries

---
+++ clean_entries.sql
@@ -0,0 +1,7 @@
+delete from user_entry_score_link;
+delete from comment;
+delete from vote;
+delete from uzer_saved_entries;
+delete from uzer_hidden_entries;
+delete from tag_entry_link;
+delete from entry;
