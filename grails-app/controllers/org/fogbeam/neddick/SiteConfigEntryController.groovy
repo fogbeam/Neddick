d1423479f20a3186393bb7823557aba38467fc40
Phillip Rhodes <motley.crue.fan@gmail.com>
2011-12-21 19:00:38 -0500

    fixes bug #5

--- grails-app/controllers/org/fogbeam/neddick/SiteConfigEntryController.groovy
+++ grails-app/controllers/org/fogbeam/neddick/SiteConfigEntryController.groovy
@@ -2,5 +2,70 @@ package org.fogbeam.neddick
 
 class SiteConfigEntryController {
 
-	def scaffold = true;
+	def scaffold = false;
+	
+	def index =
+	{
+		redirect(controller:"siteConfigEntry", action:"list");
+	}
+
+	def list =
+	{
+		List<SiteConfigEntry> allEntries = SiteConfigEntry.findAll();
+		[allEntries:allEntries];	
+	}
+
+	def edit =
+	{
+		SiteConfigEntry theEntry = SiteConfigEntry.findById( params.id );
+		
+		[ theEntry: theEntry ];	
+	}
+	
+	def create = 
+	{
+		
+		[];	
+	}
+	
+	def save =
+	{
+		SiteConfigEntry theEntry = new SiteConfigEntry();
+		theEntry.name = params.entryName;
+		theEntry.value = params.entryValue;
+		
+		if( !theEntry.save())
+		{
+			flash.message = "Failed to save entry!";
+			theEntry.errors.allErrors.each { println it };
+		}
+		
+		redirect( controller:"siteConfigEntry", action:"list" );
+	}
+			
+	def update =
+	{
+		println params;
+		
+		SiteConfigEntry theEntry = SiteConfigEntry.findById( params.entryId );
+		theEntry.name = params.entryName;
+		theEntry.value = params.entryValue;
+		
+		if( !theEntry.save())
+		{
+			flash.message = "Failed to save entry!";	
+			theEntry.errors.allErrors.each { println it };
+		}
+
+		redirect( controller:"siteConfigEntry", action:"list" );	
+	}
+	
+	def delete =
+	{
+		SiteConfigEntry theEntry = SiteConfigEntry.findById( params.entryId );
+		theEntry.delete();
+		
+		redirect( controller:"siteConfigEntry", action:"list" );
+	}
+	
 }
---
+++ src/groovy/org/fogbeam/neddick/siteconfig/SiteConfigConstants.groovy
@@ -0,0 +1,8 @@
+package org.fogbeam.neddick.siteconfig
+
+class SiteConfigConstants {
+
+	public static final LUCENE_INDEX_PATH = "";
+	
+	
+}
