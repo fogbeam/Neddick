12b1ae4ca2df3e3c7833ac39900e1662d8df55d2
Robert Fischer <robert.fischer@smokejumperit.com>
2012-07-01 18:39:20 -0400

    Upgrade to 2.0.4, introduction of BuildConfig.groovy

---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,40 @@
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
+//grails.project.war.file = "target/${appName}-${appVersion}.war"
+
+grails.project.dependency.resolution = {
+    // inherit Grails' default dependencies
+    inherits("global") {
+        // uncomment to disable ehcache
+        // excludes 'ehcache'
+    }
+    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    repositories {
+				mavenLocal()
+				ebr()
+        grailsPlugins()
+        grailsHome()
+        grailsCentral()
+
+        // uncomment the below to enable remote dependency resolution
+        // from public Maven repositories
+        mavenCentral()
+        //mavenRepo "http://snapshots.repository.codehaus.org"
+        //mavenRepo "http://repository.codehaus.org"
+        //mavenRepo "http://download.java.net/maven/2/"
+        //mavenRepo "http://repository.jboss.com/maven2/"
+    }
+    dependencies {
+        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+
+        // runtime 'mysql:mysql-connector-java:5.1.13'
+      runtime 'postgresql:postgresql:9.1-901.jdbc4'
+    }
+		plugins {
+			compile ":feeds:1.5"
+			compile ":jms:1.2"
+			compile ":quartz:1.0-RC2"
+			compile ":navigation:1.3.2"
+		}
+}
