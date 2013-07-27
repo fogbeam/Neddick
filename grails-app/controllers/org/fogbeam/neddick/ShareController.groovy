8b50dc69ff0af53fffc1eebca6cc36b172606ca0
Phillip Rhodes (mindcrime) <motley.crue.fan@gmail.com>
2013-07-26 20:42:17 -0400

    remove obsolete method from ShareController

--- grails-app/controllers/org/fogbeam/neddick/ShareController.groovy
+++ grails-app/controllers/org/fogbeam/neddick/ShareController.groovy
@@ -118,41 +118,4 @@ class ShareController
 		
 		render( "OK" );
 	}
-	
-	def shareEntry = {
-	
-		def entryId = params.entryId;
-		def entryToSend = Entry.findById( Integer.parseInt ( entryId ) );
-		
-		String shareTo = params.share_to;
-		String[] addresses = shareTo.trim().split("\\s+");
-
-		def messageText = "${entryToSend.title} \n ${entryToSend.url}\n";
-		def messageSubject = "Shared Entry from Project Shelly, shared by ${session.user?.userId}";
-		
-		for( String address in addresses ) 
-		{
-			log.debug( "Sharing to address: ${address}");
-			
-			if( address.startsWith( "xmpp:"))
-			{
-				String[] parts = address.split( "xmpp:");
-				xmppNotificationService.sendChat( parts[1], messageSubject + "\n" + messageText );
-			}
-			else
-			{
-			
-				// send this entry to this address
-				SimpleMailMessage message = new SimpleMailMessage(mailMessage);
-				String[] to = [address];
-				message.to = to;
-				log.debug( "\n\n" + message.to + "\n\n" );
-			
-				message.text = messageText;
-				message.subject = messageSubject; 
-				mailSender.send( message );
-			}
-		}
-	}
-	
 }
