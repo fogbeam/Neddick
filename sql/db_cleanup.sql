ff6d045e54c7656928ed69ab87513306acb9b859
Phillip Rhodes (mindcrime) <motley.crue.fan@gmail.com>
2013-08-24 17:57:13 -0400

    add a sql file for cleaning the database (for development use)

---
+++ sql/db_cleanup.sql
@@ -0,0 +1,7 @@
+﻿delete from base_filter_entry;
+delete from base_filter_criteria;
+delete from base_filter;
+delete from user_entry_score_link;
+delete from channel_entry_link;
+delete from entry;
+delete from uzer;
