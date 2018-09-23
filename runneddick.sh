0b1191b1149155e8697bdfad16a51c7b53d52882
Phillip Rhodes <motley.crue.fan@gmail.com>
2018-09-22 22:38:57 -0400

    remove existing neddick.log in this startup script

--- runneddick.sh
+++ runneddick.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+rm /opt/fogcutter/neddick/neddick.log
+
 ./grailsw clean; ./grailsw -Dserver.port=8282 -Dneddick.home=/opt/fogcutter/neddick -Dspring.config.location=/opt/fogcutter/neddick/ -Drebuild.indexes=false -Dfogbeam.devmode=true run-app $@
